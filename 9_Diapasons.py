8aba77587a4f86ec4ecdfce6ad4729847d21b215
SingingFoot <singingfoot@gmail.com>
2020-07-13 10:23:43 +0300

    Add 9_Diapasons.py

---
+++ 9_Diapasons.py
@@ -0,0 +1,24 @@
+numbers = list(range(0, 101, 10))
+print(numbers)
+
+nums = list(range(3, 15, 3))
+print(nums[2])
+
+
+numbers = [0, 1, 2, 3, 4, 5, 6]
+i = 0
+length = len(numbers) - 1
+while i <= length:
+    print (numbers[i])
+    i += 1
+
+numbers = [1, 2, 3, 4]
+for e in numbers:
+    print(str(e))
+
+for t in range(3):
+    print("Hi, dude!")
+
+list = [1,1,2,3,5,8,13]
+print(list[list[4]])
+    
